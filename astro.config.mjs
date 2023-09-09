3bcc203b29f6ef51f08f1cdced04c7ab2bdb082b
bmark210 <baltenco@yandex.ru>
2023-09-09 11:54:27 +0300

    [add] localization(en, ru)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -11,6 +12,7 @@ export default defineConfig({
   integrations: [
     sitemap(),
     tailwind(),
+    astroI18next(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
       cacheDir: "./.cache/image",
